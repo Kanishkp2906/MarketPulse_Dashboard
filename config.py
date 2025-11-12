5f1dcb82c77fcd4737bcc0af7aa97daf84beb287
Kanishk@29 <kanishkprasad6@gmail.com>
2025-11-12 23:44:49 +0530

    Updated config.py with streamlit secrets

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import streamlit as st
 
 # Environment variable loaders to get the API keys and variables.
-metal_price_api_key = os.getenv('metal_price_api_key')
-usd_rate_api_key = os.getenv('usd_rate_api_key')
-troy_ounce = os.getenv('troy_ounce')
-stocks_data_api_key = os.getenv('stocks_data_api_key')
+metal_price_api_key = st.secrets['metal_price_api_key']
+usd_rate_api_key = st.secrets['usd_rate_api_key']
+troy_ounce = st.secrets['troy_ounce']
+stocks_data_api_key = st.secrets['stocks_data_api_key']
