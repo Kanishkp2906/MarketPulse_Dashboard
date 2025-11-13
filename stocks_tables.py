edbde4971e25cc04ffc3cf62f45e2c2df8425b8d
Kanishk@29 <kanishkprasad6@gmail.com>
2025-11-13 13:47:32 +0530

    Removed the None value from the stocks symbols.

--- stocks_tables.py
+++ stocks_tables.py
@@ -1,7 +1,6 @@
 #from database import db_bse_dataframe, db_nse_dataframe
 # Hard coded stock symbols to fetch the historical data.
 stock_symbols = [
-    None,
     "reliance",
     'tcs',
     'hdfcbank',
